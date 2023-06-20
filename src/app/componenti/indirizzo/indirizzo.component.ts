fd3913ac6cc3ef95cae0524e924e46e3fad72874
Davide Di Gregorio <davide.digregorio@eng.it>
2023-06-20 12:30:51 +0200

    Corretta gestione custom validator per campi indirizzo

--- src/app/componenti/indirizzo/indirizzo.component.ts
+++ src/app/componenti/indirizzo/indirizzo.component.ts
@@ -1,6 +1,10 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { checkAddressField, checkDateConsistency } from 'src/app/shared/custom.validator';
+import {
+  checkAddressField,
+  checkDateConsistency,
+} from 'src/app/shared/custom.validator';
 
 @Component({
   selector: 'app-indirizzo',
@@ -13,7 +17,6 @@ export class IndirizzoComponent implements OnInit {
 
   constructor(public formBuilder: FormBuilder) {}
 
-
   ngOnInit(): void {
     this.addressFormGroup.statusChanges.subscribe((value) => {
       console.log('IndirizzoForm Status', value);
@@ -21,30 +24,29 @@ export class IndirizzoComponent implements OnInit {
   }
 
   public createGroup() {
-    this.addressFormGroup = this.formBuilder.group({
-      via: [''],
-      citta: [''],
-      numero: [''],
-    }, {validators : [checkAddressField('via','citta','numero')]}  );//Validatore custom per cui tutti e tre i campi sono obbligatori se almeno uno è valorizzato
+    this.addressFormGroup = this.formBuilder.group(
+      {
+        via: [''],
+        citta: [''],
+        numero: [''],
+      },
+      { validators: [checkAddressField('via', 'citta', 'numero')] }
+    ); //Validatore custom per cui tutti e tre i campi sono obbligatori se almeno uno è valorizzato
 
     return this.addressFormGroup;
   }
 
-
-  checkAddressFieldMissing() : boolean
-  {
-    //console.log('addressFormGroup', this.addressFormGroup)
-    //console.log('addressFormGroup.valid', this.addressFormGroup.valid)
-    if(this.addressFormGroup!.errors){
-
-      if (this.addressFormGroup!.errors!['addressFieldMissing']){
-        console.log('test', this.addressFormGroup!.errors!['addressFieldMissing'])
-        return true;
-      }
+  checkAddressFieldMissing(field: string): boolean {
+    if (
+      this.addressFormGroup &&
+      this.addressFormGroup.controls &&
+      this.addressFormGroup.controls[field] &&
+      this.addressFormGroup.controls[field].errors &&
+      this.addressFormGroup.controls[field].errors!['addressFieldMissing']
+    ) {
+      return true;
     }
 
     return false;
   }
-
-
 }
--- src/app/shared/custom.validator.ts
+++ src/app/shared/custom.validator.ts
@@ -18,9 +18,25 @@ export function checkAddressField(
     //Se uno dei tre campi è valorizzato allora devono esserlo tutti e tre
     if (firstFieldValue || secondFieldValue || thirdFieldValue) {
       if (!firstFieldValue || !secondFieldValue || !thirdFieldValue) {
+        if (!firstFieldValue)
+          control.get(firstField)?.setErrors({ addressFieldMissing: true });
+        else control.get(firstField)?.setErrors(null);
+
+        if (!secondFieldValue)
+          control.get(secondField)?.setErrors({ addressFieldMissing: true });
+        else control.get(secondField)?.setErrors(null);
+
+        if (!thirdFieldValue)
+          control.get(thirdField)?.setErrors({ addressFieldMissing: true });
+        else control.get(thirdField)?.setErrors(null);
+
         return { addressFieldMissing: true };
       }
     }
+
+    control.get(firstField)?.setErrors(null);
+    control.get(secondField)?.setErrors(null);
+    control.get(thirdField)?.setErrors(null);
 
     return null; //Validazione superata
   };
@@ -30,18 +46,14 @@ export function checkDateConsistency(
   firstField: string,
   secondField: string
 ): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors| null => {
-    const firstDate : Date = control.get(firstField)?.value;
-    const secondDate : Date = control.get(secondField)?.value;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const firstDate: Date = control.get(firstField)?.value;
+    const secondDate: Date = control.get(secondField)?.value;
 
+    if (firstDate && secondDate) {
+      const isRangeValid = secondDate.getTime() - firstDate.getTime() > 0;
 
-    if(firstDate && secondDate)
-    {
-      const isRangeValid = (secondDate.getTime() - firstDate.getTime() > 0);
-
-      return isRangeValid ? null : {dateLessThan : true}
-
-
+      return isRangeValid ? null : { dateLessThan: true };
     }
     return null; //Validazione superata
   };
